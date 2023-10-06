67f09f95b15ffb0f696219650f449e94bc5e0d0b
Md Ashik Ansari <aashiqmohd04@gmail.com>
2023-10-06 21:26:27 +0530

    Created controllers for getting a task by id, update task by id, delete task by id

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -60,7 +60,121 @@ const getAllTasks = async (req, res) => {
   }
 };
 
+// Controller to retrieve a specific task by ID
+const getTaskById = async (req, res) => {
+  try {
+    // Get the task ID from the URL parameters
+    const taskId = req.params.taskId;
+
+    // Find the task by ID
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task Not Found" });
+    }
+    // Check if the user is the task creator
+    if (task.user.toString() !== req.body.user) {
+      return res.status(401).json({ message: "Not Authorized" });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+// Controller to update a specific task by ID
+const updateTaskById = async (req, res) => {
+  try {
+    const { title, description, status } = req.body;
+    // Get the task ID from the URL parameters
+    const taskId = req.params.taskId;
+
+    // Find the task by ID
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task Not Found" });
+    }
+
+    // Joi schema for validating the request data
+    const taskUpdateSchema = Joi.object({
+      title: Joi.string(),
+      description: Joi.string(),
+      status: Joi.string().valid("pending", "completed"),
+    });
+
+    // Validate and update task data
+    const { error } = taskUpdateSchema.validate({
+      title,
+      description,
+      status,
+    });
+
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+
+    // Check if the user is the task creator
+    if (task.user.toString() !== req.body.user) {
+      return res.status(401).json({ message: "Not Authorized" });
+    }
+
+    // Update task fields based on the request body
+    if (req.body.title) {
+      task.title = req.body.title;
+    }
+    if (req.body.description) {
+      task.description = req.body.description;
+    }
+    if (req.body.status) {
+      task.status = req.body.status;
+    }
+
+    await task.save();
+
+    res.status(200).json({ message: "Task Updated Successfully", task });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+// Controller to delete a specific task by ID
+const deleteTaskById = async (req, res) => {
+  try {
+    // Get the task ID from the URL parameters
+    const taskId = req.params.taskId;
+
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task Not Found" });
+    }
+
+    // Check if the user is the task creator (optional)
+    if (task.user.toString() !== req.body.user) {
+      return res.status(401).json({ message: "Not Authorized" });
+    }
+
+    // Delete the task
+    await Task.deleteOne({ _id: taskId });
+
+    res.status(200).json({ message: "Task Deleted Successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+};
+
 module.exports = {
   addTask,
   getAllTasks,
+  getTaskById,
+  updateTaskById,
+  deleteTaskById,
 };
--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { addTask, getAllTasks } = require("../controllers/TaskController");
+const {
+  addTask,
+  getAllTasks,
+  getTaskById,
+  updateTaskById,
+  deleteTaskById,
+} = require("../controllers/TaskController");
 
 const taskRouter = express.Router();
 
 taskRouter.post("/task", addTask);
 taskRouter.get("/task", getAllTasks);
+taskRouter.get("/task/:taskId", getTaskById);
+taskRouter.put("/task/:taskId", updateTaskById);
+taskRouter.delete("/task/:taskId", deleteTaskById);
 
 module.exports = { taskRouter };
